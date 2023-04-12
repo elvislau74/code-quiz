fd30715e3ca43af21c4416b7bff7232a59ac1993
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-04-12 17:17:07 -0400

    add variables in js for score, time, high scores and add variables grabbing classes in the html using query selectors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 var startEL = document.querySelector(".select");
+var timeEL = document.querySelector(".time");
+var scoreEL = document.querySelector(".score");
 
 
 var highScores = [];
+var score = 0;
+var time = 60;
+var timerInterval = 0;
