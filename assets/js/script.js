f358e01006d78f33d9728936192ab909ba6494ba
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-04-12 19:17:41 -0400

    adjust html and add styling to answer buttons, create variables to connect to question and answer buttons in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 var startEL = document.querySelector(".select");
 var timeEL = document.querySelector(".time");
 var scoreEL = document.querySelector(".score");
+var questionEL = document.querySelector(".question-container");
+var answer1 = document.querySelectorAll(".answer-1");
+var answer2 = document.querySelectorAll(".answer-2");
+var answer3 = document.querySelectorAll(".answer-3");
+var answer4 = document.querySelectorAll(".answer-4");
 
 
 var highScores = [];
